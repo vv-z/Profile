e43f5ad4b4a4e2020004944a034eddcba3cace57
Vitaliy Z <vv-z@mail.ru>
2023-04-02 14:03:00 +0300

    Initinal commit

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class FormDate {
+
+    public int day;
+    public int month;
+    public int year;
+
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public ru.netology.FormDate birthday;
+
+}
